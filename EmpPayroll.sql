44feca3ad02db9b4b58217e5da997ec059f28102
Diwakar <diwakar.n@tvsnext.io>
2021-08-01 12:55:50 +0530

    [Diwakar] Create table

--- EmpPayroll.sql
+++ EmpPayroll.sql
@@ -1,5 +1,12 @@
----UC1----
---Create db--
-create database Payroll_Services;
+create database payroll_services;
 
 use Payroll_Services;
+--Uc2---
+---Create Table---
+create table employee_payroll(
+	empId int identity(1,1) primary key,
+	name varchar(20) not null,
+	salary float,
+	startDate date,
+	emailId varchar(20)
+);
