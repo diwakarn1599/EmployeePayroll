7e41010c24911692cfe8d08501a2d04627f26cbf
Diwakar <diwakar.n@tvsnext.io>
2021-08-01 13:18:58 +0530

    [Diwakar] Select data from table

--- EmpPayroll.sql
+++ EmpPayroll.sql
@@ -17,3 +17,7 @@ create table employee_payroll
 --Insert data--
 Insert into employee_payroll(name,salary,startDate,emailId) values ('Diwakar',25000,'2021-07-01','asdf@asdf.com'); 
 Insert into employee_payroll values ('Ajith',30000,'2021-08-01','ajith@asdf.com'),('Vijay',32000,'2021-09-01','ajith@asdf.com'),('Rajini',40000,'2018-10-03','rajini@asdf.com');  
+
+--Uc4--
+--Select data--
+Select * from employee_payroll;
