d1b51e5c1f8fd30aca1ddce38b15474c28e19efe
Diwakar <diwakar.n@tvsnext.io>
2021-08-02 12:08:14 +0530

    [Diwakar] Performed aggregate functions

--- EmpPayroll.sql
+++ EmpPayroll.sql
@@ -31,9 +31,22 @@ Select name,salary from employee_payroll where startDate between ('2021-05-01') 
 --Alter and update data--
 alter table employee_payroll add Gender char(1);
 
+
 --Update--
 update employee_payroll set Gender='M' where name='Diwakar' or name='Ajith' or name='Vijay' or name='Rajini';
 update employee_payroll set Gender='F' where name='Nayantara';
 
 select * from employee_payroll;
-
+Insert into employee_payroll values ('Trisha',20000,'2021-08-01','nayan@asdf.com','F');
+--UC7--
+--Aggregate Functions--
+--Min--
+Select Gender,Min(salary)as MinSalary from employee_payroll where Gender='F' group by Gender;
+--Max--
+ Select Gender,Max(salary)as MaxSalary from employee_payroll where Gender='F' group by Gender;
+ --Sum--
+ Select Gender,Sum(salary)as TotalSalary from employee_payroll where Gender='F' group by Gender;
+ --Avg--
+ Select Gender,Avg(salary)as AvgSalary from employee_payroll where Gender='F' group by Gender;
+ --Count--
+ Select Gender, count(Gender)as Count from employee_payroll group by Gender order by Gender DESC;
